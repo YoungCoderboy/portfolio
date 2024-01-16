5591f3bd8d98c53903c4cf909af8fc13f162e598
YoungCoderboy <91416534+YoungCoderboy@users.noreply.github.com>
2024-01-16 18:41:42 +0530

    Nav bar added

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import Nav from "./component/Nav";
+
+export default function App() {
+  return <Nav />;
+}
