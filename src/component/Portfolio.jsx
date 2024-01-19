9bd5122548d4768bb7f5b28e9ee99aedbf98d682
YoungCoderboy <91416534+YoungCoderboy@users.noreply.github.com>
2024-01-19 16:34:21 +0530

    change in image anchor tag

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -50,11 +50,13 @@ const Portfolio = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           {portfolios.map(({ id, src, link }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                alt=""
-                className="rounded-md duration-200 hover:scale-105"
-              />
+              <a href={link}>
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105"
+                />
+              </a>
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
